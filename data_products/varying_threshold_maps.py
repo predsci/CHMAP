4668c713630dde3d5c5467bcdf9563ee67872feb
Tamar Ervin <tamarervin@gmail.com>
2020-08-24 12:16:17 -0700

    TE: outline for threshold maps

---
+++ data_products/varying_threshold_maps.py
@@ -0,0 +1,17 @@
+"""
+code to create maps with Gaussian varying threshold values
+"""
+
+#### STEP ONE: SELECT IMAGES
+
+#### STEP TWO: APPLY CORRECTIONS
+
+#### STEP THREE: CORONAL HOLE DETECTION
+
+# use gaussian varying for threshold
+
+#### STEP FOUR: CREATE SINGLE MAPS
+
+#### STEP FIVE: CREATE COMBINED MAP
+
+#### STEP SIX: SAVE TO DATABASE
