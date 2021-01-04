7ca9f188e2c8d0e9f39aa65a825b9a63df40548c
opalissan <oissan@predsci.com>
2021-01-04 14:15:39 -0800

    test access.

--- analysis/ml_analysis/training_unet.py
+++ analysis/ml_analysis/training_unet.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append("/Users/tamarervin/CH_Project/CHD")
+# sys.path.append("/Users/tamarervin/CH_Project/CHD")
 import numpy as np
 import h5py as h5
 import numpy.random as random
