90325b744702a25bc2a8daba46ec45038fb6e193
Tamar <53492021+tamarervin@users.noreply.github.com>
2021-03-04 10:33:53 -0800

    Update map_manip.py
    - CHD map added to PSI map object

--- modules/map_manip.py
+++ modules/map_manip.py
@@ -124,13 +124,12 @@ def get_1d_mesh_properties(x):
     return xh, dx, dxh
 
 
-def combine_maps(map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=None, del_mu=None):
+def combine_maps(map_list, mu_cutoff=0.0, mu_merge_cutoff=None, del_mu=None):
     """
     Take a list of Psi_Map objects and do minimum intensity merge to a single map.
     Using mu_merge_cutoff: based off the two cutoff algorithm from Caplan et. al. 2016.
     Using del_mu: based off maximum mu value from list
     :param map_list: List of Psi_Map objects
-    :param chd_map_list: List of Psi_Map objects of CHD data
     :param mu_cutoff: data points/pixels with a mu less than mu_cutoff will be discarded before
     merging.
     :param mu_merge_cutoff: mu cutoff value for discarding pixels in areas of instrument overlap
@@ -140,10 +139,7 @@ def combine_maps(map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=Non
     # determine number of maps. if only one, do nothing
     nmaps = len(map_list)
     if nmaps == 1:
-        if chd_map_list is not None:
-            return map_list[0], chd_map_list[0]
-        else:
-            return map_list[0]
+        return map_list[0]
 
     # check that all maps have the same x and y grids
     same_grid = all(map_list[0].x == map_list[1].x) and all(map_list[0].y == map_list[1].y)
@@ -165,12 +161,12 @@ def combine_maps(map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=Non
         mu_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_MU)
         data_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
         image_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_ORIGIN_IMAGE)
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             chd_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
             for ii in range(nmaps):
                 mu_array[:, :, ii] = map_list[ii].mu
                 data_array[:, :, ii] = map_list[ii].data
-                chd_array[:, :, ii] = chd_map_list[ii].data
+                chd_array[:, :, ii] = map_list[ii].chd
                 image_array[:, :, ii] = map_list[ii].origin_image
         else:
             for ii in range(nmaps):
@@ -215,33 +211,26 @@ def combine_maps(map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=Non
         keep_mu = mu_array[row_index, col_index, map_index]
         keep_data = data_array[row_index, col_index, map_index]
         keep_image = image_array[row_index, col_index, map_index]
-
-        # Generate new CHD map
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             keep_chd = chd_array[row_index, col_index, map_index]
-            chd_map = psi_d_types.PsiMap(keep_chd, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                         origin_image=keep_image, no_data_val=map_list[0].no_data_val)
-        else:
-            chd_map = None
+
         # Generate new EUV map
         euv_map = psi_d_types.PsiMap(keep_data, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                     origin_image=keep_image, no_data_val=map_list[0].no_data_val)
-
+                                     origin_image=keep_image, chd=keep_chd, no_data_val=map_list[0].no_data_val)
     else:
         raise ValueError("'map_list' maps have different grids. This is not yet supported in " +
                          "map_manip.combine_maps()")
 
-    return euv_map, chd_map
+    return euv_map
 
 
-def combine_cr_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=0.4):
+def combine_cr_maps(n_images, map_list, mu_cutoff=0.0, mu_merge_cutoff=0.4):
     """
         Take an already combined map, and a single image map, and do minimum intensity merge to a single map.
         Using mu_merge_cutoff: based off the two cutoff algorithm from Caplan et. al. 2016.
         Using del_mu: based off maximum mu value from list
         :param n_images: number of images in original map
         :param map_list: List of Psi_Map objects (single_euv_map, combined_euv_map)
-        :param chd_map_list: List of Psi_Map objects of CHD data (single_chd_map, combined_chd_map)
         :param mu_cutoff: data points/pixels with a mu less than mu_cutoff will be discarded before
         merging.
         :param mu_merge_cutoff: mu cutoff value for discarding pixels in areas of instrument overlap
@@ -250,10 +239,7 @@ def combine_cr_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
     # determine number of maps. if only one, do nothing
     nmaps = len(map_list)
     if nmaps == 1:
-        if chd_map_list is not None:
-            return map_list[0], chd_map_list[0]
-        else:
-            return map_list[0]
+        return map_list[0]
 
     # check that all maps have the same x and y grids
     same_grid = all(map_list[0].x == map_list[1].x) and all(map_list[0].y == map_list[1].y)
@@ -330,11 +316,11 @@ def combine_cr_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
         keep_image = image_array[row_index, col_index, map_index]
 
         # Generate new CHD map
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             chd_data = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
             chd_array = np.ndarray(shape=mat_size, dtype=DTypes.MAP_DATA)
             for ii in range(nmaps):
-                chd_data[:, :, ii] = chd_map_list[ii].data
+                chd_data[:, :, ii] = map_list[ii].chd
             # determine overlap pixels
             use_chd = np.logical_and(map_list[0].data != map_list[0].no_data_val,
                                      map_list[1].data != map_list[0].no_data_val)
@@ -350,29 +336,26 @@ def combine_cr_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
             chd_array[use_org] = chd_data[:, :, 1][use_org]
             # choose correct chd data to use
             keep_chd = chd_array[row_index, col_index]
-            chd_combined = psi_d_types.PsiMap(keep_chd, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                              origin_image=keep_image, no_data_val=map_list[0].no_data_val)
         else:
-            chd_combined = None
+            keep_chd = None
         # Generate new EUV map
         euv_combined = psi_d_types.PsiMap(keep_data, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                          origin_image=keep_image, no_data_val=map_list[0].no_data_val)
+                                          origin_image=keep_image, chd=keep_chd, no_data_val=map_list[0].no_data_val)
 
     else:
         raise ValueError("'map_list' maps have different grids. This is not yet supported in " +
                          "map_manip.combine_cr_maps()")
 
-    return euv_combined, chd_combined
+    return euv_combined
 
 
-def combine_mu_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_merge_cutoff=0.4):
+def combine_mu_maps(n_images, map_list, mu_cutoff=0.0, mu_merge_cutoff=0.4):
     """
         Take an already combined map, and a single image map, and do minimum intensity merge to a single map.
         Using mu_merge_cutoff: based off the two cutoff algorithm from Caplan et. al. 2016.
         Using del_mu: based off maximum mu value from list
         :param n_images: number of images in original map
         :param map_list: List of Psi_Map objects (single_euv_map, combined_euv_map)
-        :param chd_map_list: List of Psi_Map objects of CHD data (single_chd_map, combined_chd_map)
         :param mu_cutoff: data points/pixels with a mu less than mu_cutoff will be discarded before
         merging.
         :param mu_merge_cutoff: mu cutoff value for discarding pixels in areas of instrument overlap
@@ -381,16 +364,12 @@ def combine_mu_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
     # determine number of maps. if only one, do nothing
     nmaps = len(map_list)
     if nmaps == 1:
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             mu_values = map_list[0].mu
             mu_values = np.where(mu_values > 0, mu_values, 0.01)
-            # map_list[0].data = map_list[0].data * mu_values
-            chd_map_list[0].data = chd_map_list[0].data * mu_values
-            return map_list[0], chd_map_list[0]
+            map_list[0].chd = map_list[0].chd * mu_values
+            return map_list[0]
         else:
-            mu_values = map_list[0].mu
-            mu_values = np.where(mu_values > 0, mu_values, 0.01)
-            # map_list[0].data = map_list[0].data * mu_values
             return map_list[0]
 
     # check that all maps have the same x and y grids
@@ -471,11 +450,11 @@ def combine_mu_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
         keep_image = image_array[row_index, col_index, map_index]
 
         # Generate new CHD map
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             chd_data = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
             chd_array = np.ndarray(shape=mat_size, dtype=DTypes.MAP_DATA)
             for ii in range(nmaps):
-                chd_data[:, :, ii] = chd_map_list[ii].data
+                chd_data[:, :, ii] = map_list[ii].chd
             # add mu weighting to new data
             chd_data[:, :, 0] = chd_data[:, :, 0] * mu_array[:, :, 0]
             # determine overlap pixels
@@ -493,30 +472,24 @@ def combine_mu_maps(n_images, map_list, chd_map_list=None, mu_cutoff=0.0, mu_mer
             chd_array[use_org] = chd_data[:, :, 1][use_org]
             # choose correct chd data to use
             keep_chd = chd_array[row_index, col_index]
-            chd_combined = psi_d_types.PsiMap(keep_chd, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                              origin_image=keep_image, no_data_val=map_list[0].no_data_val)
         else:
-            chd_combined = None
+            keep_chd = None
         # Generate new EUV map
         euv_combined = psi_d_types.PsiMap(keep_data, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                          origin_image=keep_image, no_data_val=map_list[0].no_data_val)
+                                          origin_image=keep_image, chd=keep_chd, no_data_val=map_list[0].no_data_val)
 
     else:
         raise ValueError("'map_list' maps have different grids. This is not yet supported in " +
                          "map_manip.combine_mu_maps()")
 
-    return euv_combined, chd_combined
+    return euv_combined
 
 
-def combine_timescale_maps(timescale_weights, map_list, chd_map_list=None):
+def combine_timescale_maps(timescale_weights, map_list):
     """
        Take a list of combined maps of varying timescales and do weighted minimum intensity merge to a single map.
         :param timescale_weights: weighting list for timescales
         :param map_list: List of Psi_Map objects (single_euv_map, combined_euv_map)
-        :param chd_map_list: List of Psi_Map objects of CHD data (single_chd_map, combined_chd_map)
-        :param mu_cutoff: data points/pixels with a mu less than mu_cutoff will be discarded before
-         merging.
-        :param mu_merge_cutoff: mu cutoff value for discarding pixels in areas of instrument overlap
         :return: Psi_Map object resulting from merge.
         """
 
@@ -524,10 +497,7 @@ def combine_timescale_maps(timescale_weights, map_list, chd_map_list=None):
     nmaps = len(map_list)
 
     if nmaps == 1:
-        if chd_map_list is not None:
-            return map_list[0], chd_map_list[0]
-        else:
-            return map_list[0]
+        return map_list[0]
 
     # check that all maps have the same x and y grids
     same_grid = all(map_list[0].x == map_list[1].x) and all(map_list[0].y == map_list[1].y)
@@ -548,12 +518,12 @@ def combine_timescale_maps(timescale_weights, map_list, chd_map_list=None):
         mu_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_MU)
         data_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
         image_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_ORIGIN_IMAGE)
-        if chd_map_list is not None:
+        if map_list[0].chd is not None:
             chd_array = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
             for ii in range(nmaps):
                 mu_array[:, :, ii] = map_list[ii].mu
                 data_array[:, :, ii] = map_list[ii].data
-                chd_array[:, :, ii] = chd_map_list[ii].data
+                chd_array[:, :, ii] = map_list[ii].chd
                 image_array[:, :, ii] = map_list[ii].origin_image
         else:
             for ii in range(nmaps):
@@ -572,34 +542,27 @@ def combine_timescale_maps(timescale_weights, map_list, chd_map_list=None):
             sum_wgt += weight
             keep_data = (keep_data + data_array[row_index, col_index, wgt_ind] * weight) / sum_wgt
             keep_mu = (keep_mu + mu_array[row_index, col_index, wgt_ind] * weight) / sum_wgt
-            if chd_map_list is not None:
+            if map_list[0].chd is not None:
                 keep_chd = (keep_chd + chd_array[row_index, col_index, wgt_ind] * weight) / sum_wgt
+            else:
+                keep_chd = None
 
-        # Generate new CHD map
-        if chd_map_list is not None:
-            chd_time_combined = psi_d_types.PsiMap(keep_chd, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                                   origin_image=None, no_data_val=map_list[0].no_data_val)
-        else:
-            chd_time_combined = None
         # generate EUV map
         euv_time_combined = psi_d_types.PsiMap(keep_data, map_list[0].x, map_list[0].y, mu=keep_mu,
-                                               origin_image=None, no_data_val=map_list[0].no_data_val)
+                                               origin_image=None, chd=keep_chd, no_data_val=map_list[0].no_data_val)
 
     else:
         raise ValueError("'map_list' maps have different grids. This is not yet supported in " +
                          "map_manip.combine_maps()")
 
-    return euv_time_combined, chd_time_combined
+    return euv_time_combined
 
 
-def combine_timewgt_maps(weight, sum_wgt, map_list, chd_map_list=None, mu_cutoff=0.0):
+def combine_timewgt_maps(weight, sum_wgt, map_list, mu_cutoff=0.0):
     nmaps = len(map_list)
     if nmaps == 1:
         sum_wgt += weight
-        if chd_map_list is not None:
-            return map_list[0], chd_map_list[0], sum_wgt
-        else:
-            return map_list[0], None, sum_wgt
+        return map_list[0], sum_wgt
 
     else:
         # check that all maps have the same x and y grids
@@ -610,16 +573,6 @@ def combine_timewgt_maps(weight, sum_wgt, map_list, chd_map_list=None, mu_cutoff
                 same_grid = same_grid & same_temp
                 if not same_grid:
                     break
-
-        else:
-            # check that all maps have the same x and y grids
-            same_grid = all(map_list[0].x == map_list[1].x) and all(map_list[0].y == map_list[1].y)
-            if nmaps > 2:
-                for ii in range(1, nmaps - 1):
-                    same_temp = all(map_list[ii].x == map_list[ii + 1].x) and all(map_list[ii].y == map_list[ii + 1].y)
-                    same_grid = same_grid & same_temp
-                    if not same_grid:
-                        break
 
         if same_grid:
             if mu_cutoff > 0.0:
@@ -648,26 +601,24 @@ def combine_timewgt_maps(weight, sum_wgt, map_list, chd_map_list=None, mu_cutoff
             use_data[use_org] = (data_array[:, :, 1][use_org] * sum_wgt) / (sum_wgt + weight)
 
             # Generate new CHD map
-            if chd_map_list is not None:
+            if map_list[0].chd is not None:
                 chd_data = np.ndarray(shape=mat_size + (nmaps,), dtype=DTypes.MAP_DATA)
                 chd_array = np.ndarray(shape=mat_size, dtype=DTypes.MAP_DATA)
                 for ii in range(nmaps):
-                    chd_data[:, :, ii] = chd_map_list[ii].data
+                    chd_data[:, :, ii] = map_list[ii].chd
                 # weight chd data by gaussian distribution
                 chd_array[overlap] = (sum_wgt * chd_data[:, :, 1][overlap] + chd_data[:, :, 0][
                     overlap] * weight) / (sum_wgt + weight)
                 # insert other non-overlap data
                 chd_array[use_new] = (chd_data[:, :, 0][use_new] * weight) / (sum_wgt + weight)
                 chd_array[use_org] = (chd_data[:, :, 1][use_org] * sum_wgt) / (sum_wgt + weight)
-                # choose correct chd data to use
-                chd_combined = psi_d_types.PsiMap(chd_array, map_list[0].x, map_list[0].y, mu=None,
-                                                  origin_image=None, no_data_val=map_list[0].no_data_val)
+
             else:
-                chd_combined = None
+                chd_array = None
             # Generate new EUV map
             euv_combined = psi_d_types.PsiMap(use_data, map_list[0].x, map_list[0].y, mu=None,
-                                              origin_image=None, no_data_val=map_list[0].no_data_val)
+                                              origin_image=None, chd=chd_array, no_data_val=map_list[0].no_data_val)
             # add weight to the sum
             sum_wgt += weight
 
-    return euv_combined, chd_combined, sum_wgt
+    return euv_combined, sum_wgt
